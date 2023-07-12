8f2f459c9536770c7d9888516a6acdcddb90e63b
João Gonçalves <joao.goncalves01@gmail.com>
2023-07-12 12:04:33 +0000

    working

--- burried_tresure/1_calculate_perimeter_solution/challenge.go
+++ burried_tresure/1_calculate_perimeter_solution/challenge.go
@@ -6,6 +6,12 @@ func CalculatePerimeter(points [][]int) float64 {
 	p := 0.0
 	for idx, point := range points {
 		if idx == 0 {
+			x1 := float64(point[0])
+			x2 := float64(point[1])
+			y1 := float64(points[len(points)-1][0])
+			y2 := float64(points[len(points)-1][1])
+			d := math.Sqrt(math.Pow(x2-x1, 2) + math.Pow(y2-y1, 2))
+			p += d
 			continue
 		}
 		x1 := float64(point[0])
