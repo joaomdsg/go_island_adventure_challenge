7f9eabbb4bec561014c474d9aedc0e8ca71b7352
João Gonçalves <joao.goncalves01@gmail.com>
2023-07-14 13:20:44 +0000

    finish second challenge

--- challenges/2_treasure_hunt/main.go
+++ challenges/2_treasure_hunt/main.go
@@ -1,1 +1,10 @@
 package main
+
+func FindTreasure(islandMap [][]int) ([][]int, error) {
+
+	// Your task: Compose a path to the treasure here
+	path := [][]int{{0, 0}}
+
+	// Your task: Return the path to the treasure or an error if the island map is invalid
+	return path, nil
+}
--- challenges/2_treasure_hunt/main_test.go
+++ challenges/2_treasure_hunt/main_test.go
@@ -1,1 +1,15 @@
 package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestFindTreasure_Simple_Path(t *testing.T) {
+	islandMap := [][]int{
+		{0, 1},
+		{1, 2}}
+	path, _ := FindTreasure(islandMap)
+	assert.Equal(t, [][]int{{0, 0}}, path, "The function should return a correct path to the treasure")
+}
